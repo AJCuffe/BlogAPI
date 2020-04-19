c9fb13600d2ca65d6a5750343c270cc7ba6a72e7
Adam Cuffe <adamcuffe28@icloud.com>
2020-04-19 17:33:08 +0100

    Added Post Routes

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -3,7 +3,23 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/posts', (req, res) => {
-    res.json({ msg: 'Hello from Posts API' })
+    res.json({ todo: 'Get all posts' })
+})
+
+router.get('/posts/:id', (req, res) => {
+    res.json({ todo: `Get one post with ID: ${req.params.id}` })
+})
+
+router.post('/posts', (req, res) => {
+    res.json({ todo: 'Add one post' })
+})
+
+router.put('/posts/:id', (req, res) => {
+    res.json({ todo: `Update post with ID: ${req.params.id}` })
+})
+
+router.delete('/posts/:id', (req, res) => {
+    res.json({ todo: `Delete post with ID: ${req.params.id}` })
 })
 
 export default router
